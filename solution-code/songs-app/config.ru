c7989309f2cd449756b23fb9b54f21540973ec6e
Ben Hulan <benhulan@gmail.com>
2016-06-06 02:43:14 -0700

    fixes solution

--- solution-code/songs-app/config.ru
+++ solution-code/songs-app/config.ru
@@ -3,6 +3,7 @@ require 'bundler'
 Bundler.require
 
 require './models/song'
+require './models/cover'
 require './app'
 
 run SongsApp
--- solution-code/songs-app/db/migrate/20160606081224_add_covers_to_songs.rb
+++ solution-code/songs-app/db/migrate/20160606081224_add_covers_to_songs.rb
@@ -1,4 +1,5 @@
 class AddCoversToSongs < ActiveRecord::Migration
   def change
+    add_reference :songs, :cover, index: true
   end
 end
